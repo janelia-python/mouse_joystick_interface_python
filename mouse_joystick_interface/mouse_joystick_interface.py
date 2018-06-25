e1f261095b7637dae16f446b4d9f178fcdd099a6
Peter Polidoro <peterpolidoro@gmail.com>
2018-06-25 13:03:46 -0400

    Added to documentation.

--- mouse_joystick_interface/mouse_joystick_interface.py
+++ mouse_joystick_interface/mouse_joystick_interface.py
@@ -36,6 +36,7 @@ class MouseJoystickInterface():
     dev = MouseJoystickInterface(use_ports=['/dev/ttyACM0','/dev/ttyACM0']) # Linux specific ports
     dev = MouseJoystickInterface(use_ports=['/dev/tty.usbmodem262471','/dev/tty.usbmodem262472']) # Mac OS X specific ports
     dev = MouseJoystickInterface(use_ports=['COM3','COM4']) # Windows specific ports
+    dev.abort_assay()
     dev.start_assay()
     '''
 
